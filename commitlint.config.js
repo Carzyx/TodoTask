5cec9843cbde042faa82bd035f98bfb1dc2e5dfb
Miguel Angel Archilla <Carzyx@users.noreply.github.com>
2025-05-05 17:25:22 +0200

    docs(conventional commit): added conventional commits in project
    added conventional commits

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+// commitlint.config.js
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    // Opcional: si quieres personalizar reglas, por ejemplo
+    // rules: {
+    //   'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'chore', 'refactor']],
+    //   'scope-empty': [2, 'never']
+    // }
+  };
